3b3deba57c75118e3123fc5ab7b7cc09560110d4
Ramneet Singh <63390014+Ramneet-Singh@users.noreply.github.com>
2021-07-10 12:17:21 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 for f in $(find testcases/ -name '*.asm'); do 
-	./mips_pms 1 $f 10 2 1 1> "$(echo "$f" | sed s/test.asm/output.txt/)"; 
+	./mips_pms 1 $f 10 2 1 1 > "$(echo "$f" | sed s/test.asm/output.txt/)"; 
 done
